1274efa3e799effab9fb3f6f95b48d1b8615f6df
DARIN JACOBSON <darinjacobson@DARINs-MacBook-Pro.local>
2018-05-24 23:44:24 -0700

    initial commit

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Event;
+use MaddHatter\LaravelFullcalendar\Facades\Calendar;
+class EventController extends Controller
+{
+    public function index()
+    {
+        $events = [];
+        $data = Event::all();
+        if($data->count()) {
+            foreach ($data as $key => $value) {
+                $events[] = Calendar::event(
+                    $value->title,
+                    true,
+                    new \DateTime($value->start_date),
+                    new \DateTime($value->end_date.' +1 day'),
+                    null,
+                    // Add color and link on event
+                    [
+                        'color' => '#ff0000',
+                        'url' => 'pass here url and any route',
+                    ]
+                );
+            }
+        }
+        $calendar = Calendar::addEvents($events);
+        return view('fullcalendar', compact('calendar'));
+    }
+}
